20e73a00208a72cf25454c4dc197937c631cc094
Aayan <slmngoyo2004@gmail.com>
2024-06-15 18:22:39 +0300

    added some changes

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(cors({optionsSuccessStatus: 200}));
 
 app.use(express.json());
 
-const timestampRouter = require('./routes/timestamp');
-app.use('/timestamp', timestampRouter);
+const timestampRouter = require('./routes/api');
+app.use('/api', timestampRouter);
 
 
 app.listen(3000, () => console.log('Server Started'));
