11f6da18b7fdd02b8d3e93b53124e6ed41dcb795
Aayan <slmngoyo2004@gmail.com>
2024-06-16 21:40:43 +0300

    changes

--- routes/api.js
+++ routes/api.js
@@ -5,12 +5,28 @@ const moment = require('moment');
 
 
 //Getting all
-router.get('/:date', (req, res) => { 
+// middleware function to parse date String
+const parseDateMiddleware = (req, res, next) => {
+   const dateString = req.query.date;
+
+   try {
+      const parsedDate = new Date(dateString);
+      if(isNaN(parsedDate.getTime())) {
+        throw new Error('Invalid Date');
+      }
+        req.parsedDate = parsedDate;
+        next();
+   } catch(error){
+    res.status(400).json({ error : error.message});
+   }
+}
+
+router.get('/:date', parseDateMiddleware, (req, res) => { 
         const date = req.params.date;
 
         //checking if date is empty 
        // if (/^\s*$/.test(date)){
-        if(!req.params.date && typeof date !== "string"){
+ /*      if(!req.params.date && typeof date !== "string"){
             const date = new Date(Date.now()).toUTCString();
             res.send({ unix: Date.now(), utc: date});
 
@@ -29,7 +45,34 @@ router.get('/:date', (req, res) => {
             res.send({ 'error': 'Invalid Date'})
       }
     
-
+*/
+if (!date) {
+    const currentDate = new Date();
+    res.json({ unix: currentDate.getTime(), utc: currentDate.toUTCString() });
+} 
+// Checking if date is provided as Unix timestamp in milliseconds
+else if (/^\d{13}$/g.test(date)) {
+    const timestamp = parseInt(date);
+    const currentDate = new Date(timestamp);
+    if (!isNaN(currentDate.getTime())) {
+        res.json({ unix: timestamp, utc: currentDate.toUTCString() });
+    } else {
+        res.json({ error: 'Invalid Date' });
+    }
+} 
+// Checking if date is provided in YYYY-MM-DD format
+else if (/^\d{4}-\d{2}-\d{2}$/g.test(date)) {
+    const currentDate = new Date(date);
+    if (!isNaN(currentDate.getTime())) {
+        res.json({ unix: currentDate.getTime(), utc: currentDate.toUTCString() });
+    } else {
+        res.json({ error: 'Invalid Date' });
+    }
+} 
+// Handle invalid date format
+else {
+    res.json({ error: 'Invalid Date' });
+}
   
 });
 
