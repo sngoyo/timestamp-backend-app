f4ba1a609f2725f9a0457fdd7b1dcbdcb23efa48
Aayan <slmngoyo2004@gmail.com>
2024-06-16 23:02:52 +0300

    changes

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const moment = require('moment');
 
 
 //middleware to handle date parsing 
@@ -28,8 +27,8 @@ const middlewareDateParser = (req, res, next) => {
             req.parsedDate = parsedDate;
         } else {
         return res.status(400).json({ error: 'Invalid Date' });
+       }
     }
-}
 
     //Checking if the date parsed is valid
     if (isNaN(req.parsedDate.getTime())) {
@@ -37,7 +36,6 @@ const middlewareDateParser = (req, res, next) => {
     }
 
     next();
-
 }
 
 
@@ -48,35 +46,4 @@ router.get('/:date?', middlewareDateParser, (req, res) => {
     res.json({ unix: parsedDate.getTime(), utc: parsedDate.toUTCString() })
 });
 
-
-
-/*
-
-router.get('/:date?', parseDateMiddleware, (req, res) => { 
-        const date = req.params.date;
-
-        //checking if date is empty 
-       // if (/^\s*$/.test(date)){
- /*      if(!req.params.date && typeof date !== "string"){
-            const date = new Date(Date.now()).toUTCString();
-            res.send({ unix: Date.now(), utc: date});
-
-            //Checking  if date is in seconds convert it to return date in UTC
-        } else if (/^\d{13}$/g.test(date )) {
-            const newtime = parseInt(date);
-            const newDate = new Date(newtime).toUTCString()
-            res.send({ unix: newtime, utc: newDate});
-
-          //if date is date not in seconds 
-        } else if(/^(?:\d{4})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2]\d|3[01])$/g.test(date)) {
-            const updatedDate = new Date(date).toUTCString();
-            res.send({ unix: Date.parse(date), utc: updatedDate});
-  
-        } else {
-            res.send({ 'error': 'Invalid Date'})
-      }
-     
-});
-
-*/
 module.exports = router;
