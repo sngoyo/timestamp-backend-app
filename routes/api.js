e9764b658f27bad81ba5f66e314f004fb375df8a
Aayan <slmngoyo2004@gmail.com>
2024-06-16 16:01:59 +0300

    changes

--- routes/api.js
+++ routes/api.js
@@ -11,19 +11,19 @@ router.get('/:date', (req, res) => {
         //checking if date is empty 
         if (/^\s*$/.test(date) && date == ""){
             const date = new Date(Date.now()).toUTCString();
-            res.send({"unix": Date.now()});
-            res.send({"utc": date});
+            res.send({unix: Date.now()});
+            res.send({utc: date});
 
             //Checking if date is in seconds convert it to return date in UTC
         } else if (/^\d{13}$/g.test(date )) {
             const newtime = parseInt(date);
             const newDate = new Date(newtime).toUTCString()
-            res.send({'unix': date, 'utc': newDate});
+            res.send({unix: date, utc: newDate});
 
           //if date is date not in seconds 
         } else if(/^(?:\d{4})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2]\d|3[01])$/g.test(date) | date == "string") {
             const updatedDate = new Date(date).toUTCString();
-            res.send({'unix':Date.parse(date), 'utc': updatedDate});
+            res.send({unix:Date.parse(date), utc: updatedDate});
         
         } else {
             res.send({'error': 'Invalid Date'})
