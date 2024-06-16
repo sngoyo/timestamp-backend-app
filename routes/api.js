5ea9d08c43faa05103c2d82f40d5d17a1615ffed
Aayan <slmngoyo2004@gmail.com>
2024-06-16 16:00:12 +0300

    changes

--- routes/api.js
+++ routes/api.js
@@ -16,8 +16,9 @@ router.get('/:date', (req, res) => {
 
             //Checking if date is in seconds convert it to return date in UTC
         } else if (/^\d{13}$/g.test(date )) {
-          const newDate = UTCDate(date);
-          res.send({'unix': date, 'utc': newDate});
+            const newtime = parseInt(date);
+            const newDate = new Date(newtime).toUTCString()
+            res.send({'unix': date, 'utc': newDate});
 
           //if date is date not in seconds 
         } else if(/^(?:\d{4})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2]\d|3[01])$/g.test(date) | date == "string") {
@@ -28,33 +29,9 @@ router.get('/:date', (req, res) => {
             res.send({'error': 'Invalid Date'})
         }
     
-    //Checking if date is not a number or 145000000000.
+
   
-})
+});
 
-const UTCDate = (secondsDate) =>{
-  //  const date = new Date(secondsDate * 1000);
-     
-  const date = new Date(secondsDate * 1000);
-    
-    const daysOfWeek = ["monday", "Tuesday", "wesday","Thursday", "saturday", "Fri", "sunday"];
-
-    const dayOfWeek = daysOfWeek[date.getDay()];
-    const year = date.getFullYear();
-    const month = date.getUTCMonth() + 1;
-    const day = date.getDate();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-
-  const utcDateString = `${dayOfWeek}, ${day} ${month} ${year} ${hours} ${minutes} ${seconds} GMT`;
-  //console.log(utcDateString);
-
- // const utcDateString = `${dayOfWeek} ${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')} ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} UTC`;
-
- 
-    return utcDateString;
- 
-}
 
 module.exports = router;
